071d5340a5d7215abca5a0d5599db199f722b827
Zhaochen He <eastnileuc@gmail.com>
2025-03-27 23:50:12 -0400

    fixed issues in get_usa_demo
    - renamed medianHHincome to median_hh_income
    - corrected population variable
    - state and county dfs are now ordered correctly

--- dev/sandbox.R
+++ dev/sandbox.R
@@ -2,6 +2,10 @@ library(choroplethrMaps)
 library(tidycensus)
 devtools::load_all()
 
+z = get_state_demographics(2019,5)
+z = get_county_demographics(2019,5)
+z = get_tract_demographics(state_name = 'AL', endyear = 2019, span = 5)
+
 get_acs_data(variable = 'B19013_001', column_idx = NULL, 
                     map = 'county', endyear = 2012, span = 5) 
 
